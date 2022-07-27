a32f0a86ca555d8370602ecfbef0c1a2fd8bcc27
James Crake-Merani <james@trutho.net>
2022-07-27 09:56:00 +0100

    Moved definition of areacode-function to the end.

--- random-telno/common.scm
+++ random-telno/common.scm
@@ -24,13 +24,6 @@
 (define all-areas
   '("0113" "0114" "0115" "0116" "0117" "0118" "0121" "0131" "0141" "0151" "0161" "020" "0191" "028" "029" "01632"))
 
-(define areacode-function (list
-                           (cons "020" format-london-number)
-                           (cons "0191" format-tyneside-number)
-                           (cons "028" format-ni-number)
-                           (cons "029" format-cardiff-number)
-                           (cons "01632" format-noarea-number)))
-
 ;; FIXME: These nested if statements aren't ideal.
 ;; FIXME: There will probably be an infinite loop if the area-code is invalid.
 (define* (format-areacode-number #:optional area-code)
@@ -59,3 +52,10 @@
 
 (define-public (format-noarea-number)
   (format #f "01632960~3,'0d" (random 999)))
+
+(define areacode-function (list
+                           (cons "020" format-london-number)
+                           (cons "0191" format-tyneside-number)
+                           (cons "028" format-ni-number)
+                           (cons "029" format-cardiff-number)
+                           (cons "01632" format-noarea-number)))
